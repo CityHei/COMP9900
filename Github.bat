29463dbb64190e61c812bd63b42498089da4200f
WCY <wcy20151298@gmail.com>
2024-06-08 18:42:05 +1000

    08-06-2024 Saturday 18:42

--- Github.bat
+++ Github.bat
@@ -1,15 +1,25 @@
 @echo off
 REM 获取当前日期和时间
-for /f "tokens=1-5 delims=.:/ " %%d in ("%date% %time%") do (
-    set year=%%d
-    set month=%%e
-    set day=%%f
-    set hour=%%g
-    set minute=%%h
+for /f "tokens=1-4 delims=/- " %%a in ("%date%") do (
+    set year=%%a
+    set month=%%b
+    set day=%%c
 )
 
+for /f "tokens=1-2 delims=: " %%a in ("%time%") do (
+    set hour=%%a
+    set minute=%%b
+)
+
+REM 确保小时和分钟是两位数格式
+if %hour% LSS 10 (set hour=0%hour%)
+if %minute% LSS 10 (set minute=0%minute%)
+
+REM 获取当前星期几
+for /f "tokens=1" %%i in ('powershell -command "(Get-Date).DayOfWeek"') do set dayOfWeek=%%i
+
 REM 格式化日期和时间作为提交信息
-set commitMessage=%year%-%month%-%day% %hour%:%minute%
+set commitMessage=%year%-%month%-%day% %dayOfWeek% %hour%:%minute%
 
 REM 添加所有更改
 echo Adding changes to the staging area...
---
+++ Proposal.md
@@ -0,0 +1,13 @@
+## Background
+1. Topic 的 PDF
+2. Week2 Client的讨论
+3. Weakness 别整特别难 (2 references)
+## User stories and sprints
+1. User stories 长得离谱进入 Appendix
+2. First sprint 截图 合理的工作强度
+3. 写了的没做完会扣分
+4. Week 8 结题 写文档
+## Technical Design
+1. 55% 和 40% 有区别
+2. 之前的作业不可以雷同
+3. 
